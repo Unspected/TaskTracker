df5f6b601cebf3ce0d9efb69f8a2115996a6e460
Pavel_Andreev <unspected777@gmail.com>
2023-01-16 17:42:12 -0800

    add new vc timer

---
+++ TaskTracker/Code/Modules/Main/TimerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TimerViewController.swift
+//  TaskTracker
+//
+//  Created by Pavel Andreev on 1/16/23.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+    
+    //MARK: - IBOoutlets
+    
+    
+    //MARK: - Variables
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    // MARK: - Outlets & Objc function
+    
+    
+    //MARK: - functions
+    override class func description() -> String {
+        return "TimeViewController"
+    }
+}
